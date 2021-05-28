96ee45912af50a46df775346a0f428dfc72e7c76
ko-suke24 <kosuke0322@gmail.com>
2021-05-29 03:46:19 +0900

    Initial Commit

---
+++ FurnitureUI/FurnitureUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureUIApp.swift
+//  FurnitureUI
+//
+//  Created by 佐藤航輔 on 2021/05/29.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
