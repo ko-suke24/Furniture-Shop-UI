2ff5d85c056b6cce93fe3ec84bdf4f5699129c37
ko-suke24 <kosuke0322@gmail.com>
2021-05-30 23:08:43 +0900

    Bar View

--- FurnitureUI/ContentView.swift
+++ FurnitureUI/ContentView.swift
@@ -8,14 +8,87 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var search: String = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color("Bg")
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack (alignment: .leading) {
+                
+                BarView()
+                
+                TafLineView()
+                    .padding()
+                
+                SearchAndScanView()
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct BarView: View {
+    var body: some View {
+        HStack {
+            Button(action: {}) {
+                Image("menu")
+                    .padding()
+                    .background(Color(.white))
+                    .cornerRadius(10.0)
+            }
+            
+            Spacer()
+            
+            Button(action: {}) {
+                Image("Profile")
+                    .resizable()
+                    .frame(width: 42, height: 42)
+                    .cornerRadius(10.0)
+            }
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct TafLineView: View {
+    var body: some View {
+        Text("Find The \nBest")
+            .font(.custom("PlayfairDisplay-Regular", size: 28))
+            .foregroundColor(Color("Primary"))
+            +   Text("Furniture!")
+            .font(.custom("PlayfairDisplay-Regular-Bold", size: 28))
+            .foregroundColor(Color("Primary"))
+    }
+}
+
+struct SearchAndScanView: View {
+    @State private var search: String = ""
+    var body: some View {
+        HStack {
+            HStack {
+                Image("Search")
+                    .padding(.trailing, 8)
+                TextField("Search Furniture", text: $search)
+            }
+            .padding(.all, 20)
+            .background(Color.white)
+            .cornerRadius(10.0)
+            .padding(.trailing)
+            
+            Button(action: {}) {
+                Image("Scan")
+                    .padding()
+                    .background(Color("Primary"))
+                    .cornerRadius(10.0)
+            }
+        }
+        .padding(.horizontal)
     }
 }
