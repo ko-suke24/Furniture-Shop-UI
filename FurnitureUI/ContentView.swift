b2b2d25f0d6e29dc662b37b15b811db1b7aaf598
ko-suke24 <kosuke0322@gmail.com>
2021-06-04 07:23:48 +0900

    UI調整

--- FurnitureUI/ContentView.swift
+++ FurnitureUI/ContentView.swift
@@ -9,86 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State private var search: String = ""
     var body: some View {
-        ZStack {
-            Color("Bg")
-                .edgesIgnoringSafeArea(.all)
-            
-            VStack (alignment: .leading) {
-                
-                BarView()
-                
-                TafLineView()
-                    .padding()
-                
-                SearchAndScanView()
-            }
+        VStack {
+            HomeScreen()
         }
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
-}
-
-struct BarView: View {
-    var body: some View {
-        HStack {
-            Button(action: {}) {
-                Image("menu")
-                    .padding()
-                    .background(Color(.white))
-                    .cornerRadius(10.0)
-            }
-            
-            Spacer()
-            
-            Button(action: {}) {
-                Image("Profile")
-                    .resizable()
-                    .frame(width: 42, height: 42)
-                    .cornerRadius(10.0)
-            }
-        }
-        .padding(.horizontal)
-    }
-}
-
-struct TafLineView: View {
-    var body: some View {
-        Text("Find The \nBest")
-            .font(.custom("PlayfairDisplay-Regular", size: 28))
-            .foregroundColor(Color("Primary"))
-            +   Text("Furniture!")
-            .font(.custom("PlayfairDisplay-Regular-Bold", size: 28))
-            .foregroundColor(Color("Primary"))
-    }
-}
-
-struct SearchAndScanView: View {
-    @State private var search: String = ""
-    var body: some View {
-        HStack {
-            HStack {
-                Image("Search")
-                    .padding(.trailing, 8)
-                TextField("Search Furniture", text: $search)
-            }
-            .padding(.all, 20)
-            .background(Color.white)
-            .cornerRadius(10.0)
-            .padding(.trailing)
-            
-            Button(action: {}) {
-                Image("Scan")
-                    .padding()
-                    .background(Color("Primary"))
-                    .cornerRadius(10.0)
-            }
-        }
-        .padding(.horizontal)
     }
 }
